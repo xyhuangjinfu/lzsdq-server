16826096d335124862b52d166a90a719bcba2005
xyhuangjinfu <630569027@qq.com>
2021-05-24 13:20:25 +0800

    init

---
+++ src/main/java/cn/hjf/lzsdq/KnowledgeRepository.java
@@ -0,0 +1,6 @@
+package cn.hjf.lzsdq;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KnowledgeRepository extends JpaRepository<Knowledge, Long> {
+}
---
+++ src/main/java/cn/hjf/lzsdq/LzsdqApplication.java
@@ -0,0 +1,12 @@
+package cn.hjf.lzsdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzsdqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzsdqApplication.class, args);
+    }
+}
