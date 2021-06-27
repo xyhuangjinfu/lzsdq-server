4412f34b5bd3e319dca7df9472cf756c94e46b2f
xyhuangjinfu <630569027@qq.com>
2021-06-27 19:34:46 +0800

    数据库连接池

--- src/main/java/cn/hjf/lzsdq/article/dao/repository/ConnectionManager.java
+++ src/main/java/cn/hjf/lzsdq/article/dao/repository/ConnectionManager.java
@@ -1,28 +1,48 @@
 package cn.hjf.lzsdq.article.dao.repository;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
 
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Connection conn = null;
+//        try {
+//            conn =
+//                    DriverManager.getConnection("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":" + System.getenv("MYSQL_PORT") + "/" + System.getenv("MYSQL_SCHEMA") + "?" +
+//                            "user=" + System.getenv("MYSQL_USER") + "&password=" + System.getenv("MYSQL_PWD"));
+//            return conn;
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+
     public static Connection getConnection() {
+        ComboPooledDataSource source = new ComboPooledDataSource();
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            source.setDriverClass("com.mysql.cj.jdbc.Driver");
+        } catch (PropertyVetoException e) {
             e.printStackTrace();
         }
-
-        Connection conn = null;
+        source.setJdbcUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":" + System.getenv("MYSQL_PORT") + "/" + System.getenv("MYSQL_SCHEMA"));
+        source.setUser(System.getenv("MYSQL_USER"));
+        source.setPassword(System.getenv("MYSQL_PWD"));
         try {
-            conn =
-                    DriverManager.getConnection("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":" + System.getenv("MYSQL_PORT") + "/" + System.getenv("MYSQL_SCHEMA") + "?" +
-                            "user=" + System.getenv("MYSQL_USER") + "&password=" + System.getenv("MYSQL_PWD"));
-            return conn;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+            return source.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
         return null;
     }
 }
