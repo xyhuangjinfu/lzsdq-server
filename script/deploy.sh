9964ac1d364e022bc1a2c624cc0dbfebe27351bb
xyhuangjinfu <630569027@qq.com>
2021-06-28 18:01:08 +0800

    部署脚本

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Hello World ! deploy "
+
+#stop server
+pid=`netstat -apn | grep 9999 | awk '{print $7}' | awk -F '/' '{print $1}'`
+if [ -z  $pid ] 
+then
+	echo 'no service'
+else
+ 	kill -9 $pid
+fi
+
+#clean and build
+cd ..
+mvn clean
+mvn package
+
+#start server
+cd target
+nohup java -jar lzsdq-1.0-SNAPSHOT.jar &
+
