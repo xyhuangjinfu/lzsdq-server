514e8f8463f58b0e92453640576f84cd2d17981b
xyhuangjinfu <630569027@qq.com>
2021-07-05 20:22:53 +0800

    完善排序规则，分数相同，大id在前

--- src/main/java/cn/hjf/lzsdq/article/biz/service/ArticleManager.java
+++ src/main/java/cn/hjf/lzsdq/article/biz/service/ArticleManager.java
@@ -76,20 +76,26 @@ public class ArticleManager {
      */
 
     private void sortByScore(List<ArticleEntity> list) {
+        DateTimeUtil dateTimeUtil = new DateTimeUtil();
         list.sort(new Comparator<ArticleEntity>() {
             @Override
             public int compare(ArticleEntity o1, ArticleEntity o2) {
-                double s1 = articleScore(o1);
-                double s2 = articleScore(o2);
+                int day1 = dateTimeUtil.betweenDaysToNow(o1.getCreateTime());
+                int day2 = dateTimeUtil.betweenDaysToNow(o2.getCreateTime());
+                if (day1 == day2 && o1.getVote().getVoteCount().equals(o2.getVote().getVoteCount())) {
+                    return o2.getId().compareTo(o1.getId());
+                }
+
+                double s1 = articleScore(o1, day1);
+                double s2 = articleScore(o2, day2);
                 return Double.compare(s2, s1);
             }
         });
     }
 
-    private double articleScore(ArticleEntity articleEntity) {
+    private double articleScore(ArticleEntity articleEntity, int days) {
         long vote = articleEntity.getVote().getVoteCount();
         vote = Math.max(vote, 1);
-        int days = new DateTimeUtil().betweenDaysToNow(articleEntity.getCreateTime());
         double score = vote / Math.pow(days + 2, 1.8);
         return score;
     }
